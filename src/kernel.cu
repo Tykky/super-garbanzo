30f404a6edb019f807da3faf5e476592d9a96b5f
Tykky <40919314+Tykky@users.noreply.github.com>
2023-02-27 15:00:03 +0200

    XD

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,9 +1,10 @@
-#include <iostream>
+#include <stdio.h>
 #include <math.h>
 #include "curand_kernel.h"
+#include <cuda.h>
 
 __global__ 
-void compute_pi(int samples, float* result, unsigned long seed)
+void compute_pi(int samples, double* result, unsigned long seed)
 {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -12,29 +13,63 @@ void compute_pi(int samples, float* result, unsigned long seed)
 
     int inside_circle = 0;
 
+    printf("thread %i launched\n", idx);
     for (size_t i = 0; i < samples; i++)
     {
         float x = curand_uniform(&state);
         float y = curand_uniform(&state);
 
-        if (x * x + y * y <= 1) // inside circle
+        if (x * x + y * y <= 1) 
             inside_circle++;
     }
 
-    result[idx] = ((float)inside_circle / (float)samples) * 4;
+    result[idx] = ((double)inside_circle / (double)samples) * 4;
+    printf("thread %i result: %.13f\n", idx, result[idx]);
 }
 
 void launch_kernel()
 {
-    long N = 1 << 28;
+    int device;
+    if (cudaGetDevice(&device) != cudaSuccess)
+    {
+        printf("Cuda capable device not foundn\n");
+        getchar();
+        return;
+    }
 
-    float* res;
-    cudaMallocManaged(&res, N * sizeof(float));
+    int ndevices;
+    cudaGetDeviceCount(&ndevices);
+
+    for (int i = 0; i < ndevices; i++)
+    {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        printf("  Number of sm: %i\n", prop.multiProcessorCount);
+        printf("  Warp size in threads: %i\n", prop.warpSize);
+        printf("  Maximum number of threads per block: %i\n", prop.maxThreadsPerBlock);
+        printf("  Maximum number of blocks per sm: %i\n", prop.maxBlocksPerMultiProcessor);
+        printf("  Maximum number of concurrent kernels: %i\n", prop.concurrentKernels);
+        printf("  Maximum number of resident threads: %i\n", prop.maxThreadsPerMultiProcessor * prop.multiProcessorCount);
+    }
+
+    printf("\n");
+    getchar();
+    
+
+    long N = 1 << 15;
+
+    double* res;
+    cudaMallocManaged(&res, N * sizeof(double));
 
     int blockSize = 256;
     int numBlocks = (N + blockSize - 1) / blockSize;
 
-    compute_pi<<<numBlocks, blockSize>>>(1 << 25, res, 420);
+    compute_pi<<<numBlocks, blockSize>>>(1 << 30, res, 420);
     cudaDeviceSynchronize();
 
     long double average = 0;
@@ -47,8 +82,6 @@ void launch_kernel()
     average /= (double)N;
     cudaFree(&res);
 
-    char asd[512];
-
-    std::cout << average << "\n";
-    std::cin >> asd;
+    printf("average: %.13f\n", average);
+    getchar();
 }
